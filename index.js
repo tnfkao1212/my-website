2ce9738db85ebc4a6ee84f5530ac5cfbcf344a67
sangwoo <tnfkao1212@naver.com>
2023-04-03 14:37:26 +0900

    테스트 커밋

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function clickBoard(boardNumber) {
     // 자유게시판
     window.location.href = '게사판';
 
-    window.location.href = '자유 게시판/free_list.html';
+    window.location.href = '게시판/free_list.html';
   } else if (boardNumber == 2) {
     //아프리카
-    window.location.href = '아프리카/af_list.html';
+    window.location.href = '게시판/af_list.html';
   } else if (boardNumber == 3) {
     //트위치
-    window.location.href = '트위치 게시판/tw_list.html';
+    window.location.href = '게시판/tw_list.html';
   }
 }
--- 트위치 게시판/twitch.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  function checkInputValue(input) {
-    if (input.val() === '') {
-      input.next('.error_box').css('display', 'block');
-    } else {
-      input.next('.error_box').css('display', 'none');
-    }
-  }
-});
