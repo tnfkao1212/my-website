c6a9d66681669d12596658f3fb60159dc0c13145
sangwoo <tnfkao1212@naver.com>
2023-04-13 09:42:33 +0900

    # 테스트 커밋

---
+++ register.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tkddn1212!',
+  database: 'test',
+  port: '3306',
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/register', (req, res) => {
+  const { id, pw, name, email } = req.body;
+
+  // 회원가입 정보 저장
+  const regist_dat = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  const level = 1;
+  const point = 0;
+
+  connection.query(
+    'INSERT INTO test (id, pw, name, email, regist_dat, level, point) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    [id, pw, name, email, regist_dat, level, point],
+    (error, results, fields) => {
+      if (error) throw error;
+      res.status(200).send('Success');
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
